3ae78db90ce17e27e79efe3a34f9d5c110e0951b
Sandeep Kapalawai <kapalawai.sandeep.sandeep@gmail.com>
2020-10-03 00:17:09 +0530

    Initial Commit

---
+++ build/precache-manifest.1ca55fbcf5ebfd477162a06a6a19f7ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5162235454507f978f540614fe34fdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3979fe5c118b8d4c33b2",
+    "url": "/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "4277fc42052231f93023",
+    "url": "/static/css/main.2ec69f44.chunk.css"
+  },
+  {
+    "revision": "3979fe5c118b8d4c33b2",
+    "url": "/static/js/2.43ffa659.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.43ffa659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4277fc42052231f93023",
+    "url": "/static/js/main.719920e6.chunk.js"
+  },
+  {
+    "revision": "9d9a4c99211d98368a0d",
+    "url": "/static/js/runtime-main.7b3982eb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
